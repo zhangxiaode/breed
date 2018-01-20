b6f655c5a790010b2121cbca4f72d0051df1f3cf
zxd <1243019627@qq.com>
2018-01-20 10:49:32 +0800

    调试

--- src/view/map.vue
+++ src/view/map.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="map">
-    map
+    <div id="mapArea" style="width:100%;height:500px;"></div>
   </div>
 </template>
 
@@ -10,6 +10,25 @@ export default {
   data () {
     return {
     }
+  },
+  mounted(){
+    //百度地图API功能
+    var map = new BMap.Map("mapArea");    // 创建Map实例118.275612,32.098167
+    map.centerAndZoom(new BMap.Point(118.160475,31.914325), 11);  // 初始化地图,设置中心点坐标和地图级别 
+    var point = new BMap.Point(118.160475,31.914325);
+    var marker = new BMap.Marker(point);        // 创建标注    
+    map.addOverlay(marker); 
+    //添加地图类型控件
+    map.addControl(new BMap.MapTypeControl({
+      mapTypes:[
+              BMAP_NORMAL_MAP,
+              BMAP_HYBRID_MAP
+          ]}));	  
+    map.setCurrentCity("北京");          // 设置地图显示的城市 此项是必须设置的
+    map.enableScrollWheelZoom(true);     //开启鼠标滚轮缩放
+    marker.addEventListener("click", function(){    
+        alert("龙虾养殖场");    
+    }); 
   }
 }
 </script>
